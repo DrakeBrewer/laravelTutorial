8d2600597e8b5b9432c0df813060c4aaa59975ca
DrakeBrewerm <drake@netstorms.com>
2022-08-22 16:20:47 -0700

    initial commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Listing;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\Models\User::factory(5)->create();
+        Listing::factory(6)->create();
+
+        // Listing::create([
+        //     'title' => 'Laravel Senior Developer',
+        //     'tags' => 'laravel, javascript',
+        //     'company' => 'Acme Corp',
+        //     'location' => 'Boston, MA',
+        //     'email' => 'email1@email.com',
+        //     'website' => 'https://www.acme.com',
+        //     'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        //     Nulla ac mi ut felis euismod vehicula eu pharetra lorem. Fusce sed 
+        //     pharetra purus, vitae porta tortor. Morbi eleifend diam at purus condimentum 
+        //     porta. Donec in elit volutpat eros ornare hendrerit quis at magna.'
+        // ]);
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Http\Controllers\ListingController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+// All listings
+Route::get('/', [ListingController::class, 'index']);
+
+
+// Show create form
+Route::get('/listings/create', [ListingController::class, 'create']);
+
+//Store Listing data
+Route::post('/listings', [ListingController::class, 'store']);
+
+// Show edit form
+Route::get('/listings/{listing}/edit', [ListingController::class, 'edit']);
+
+// Update listing
+Route::put('/listings/{listing}', [ListingController::class, 'update']);
+
+
+
+//Single Listing
+Route::get('/listings/{listing}', [ListingController::class, 'show']);
